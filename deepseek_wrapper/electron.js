59050b3176c420fea6926a84e52dc49bb4cc6d2e
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2025-03-19 15:19:10 +0530

    set up electron for desktop app

---
+++ deepseek_wrapper/electron.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("http://localhost:8081"); // Expo Web URL
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
